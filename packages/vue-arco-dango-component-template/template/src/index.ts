80582cab8743fa24a45058bb8d9d6ce670e55801
guochao <463561248@qq.com>
2022-03-22 11:27:25 +0800

    ✨ feat:  调整模板结构

--- packages/vue-arco-dango-component-template/template/src/index.ts
+++ packages/vue-arco-dango-component-template/template/src/index.ts
@@ -1,23 +1,16 @@
 /*
  * @Author: saber
  * @Date: 2022-02-18 10:12:22
- * @LastEditTime: 2022-02-23 14:59:06
+ * @LastEditTime: 2022-03-22 11:24:38
  * @LastEditors: saber
- * @Description: 
+ * @Description:
  */
 // TODO: 后面会添加 install 方法
-import FadeTransition from './Fade/FadeTransition.vue';
-import SlideYUpTransition from './Slide/SlideYUpTransition.vue';
-import SlideYDownTransition from './Slide/SlideYDownTransition.vue';
-import SlideXLeftTransition from './Slide/SlideXLeftTransition.vue';
-import SlideXRightTransition from './Slide/SlideXRightTransition.vue';
-import ZoomCenterTransition from './Zoom/ZoomCenterTransition.vue';
+import { App, Plugin } from "vue";
+import Vue3TabsChrome from "./vue3-tabs-chrome.vue";
+const install = (app: App) => {
+  app.component("Vue3TabsChrome", Vue3TabsChrome);
+};
 
-export {
-    FadeTransition,
-    SlideYUpTransition,
-    SlideYDownTransition,
-    SlideXLeftTransition,
-    SlideXRightTransition,
-    ZoomCenterTransition
-}
+Vue3TabsChrome.install = install;
+export default Vue3TabsChrome as unknown as Plugin;
