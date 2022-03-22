7746985c836eb47d834d566f9c42357778a56aa3
guochao <463561248@qq.com>
2022-03-22 19:43:23 +0800

    ✨ feat:  @dangojs/vue-arco-dango-component-template
    模板的调整 支持 tsx

--- packages/vue-arco-dango-component-template/template/stories/index.stories.js
+++ packages/vue-arco-dango-component-template/template/stories/index.stories.js
@@ -2,6 +2,15 @@ import '../src/style';
 
 export * from '../src/demo';
 
+// import TooltipButton from '../src/TooltipButton.vue';
 export default {
   title: '@CONST_PACKAGE_NAME@',
+  // component: TooltipButton,
+  // argTypes: {
+  //   tip: {
+  //     control: {
+  //       type: 'text'
+  //     }
+  //   },
+  // }, 
 };
