578cf5f48d5908dcdb4dc68d9eff0e875f07c599
guochao <463561248@qq.com>
2022-03-22 11:40:45 +0800

    arco-cli: initialize vue-defalut

---
+++ packages/vue-arco-dango-component-template/template/stories/index.stories.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: saber
+ * @Date: 2022-03-22 11:32:56
+ * @LastEditTime: 2022-03-22 11:39:42
+ * @LastEditors: saber
+ * @Description: 
+ */
+import '../src/style';
+
+export * from '../src/demo';
+
+export default {
+  title: '@CONST_PACKAGE_NAME@',
+};
