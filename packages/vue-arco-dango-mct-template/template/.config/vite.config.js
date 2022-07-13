bf52c6d3c5d118df591d862a8490fb875c0826be
guochao <463561248@qq.com>
2022-07-13 15:03:33 +0800

    ✨ feat:  增加组件构建的时候忽略deep的依赖

--- packages/vue-arco-dango-mct-template/template/.config/vite.config.js
+++ packages/vue-arco-dango-mct-template/template/.config/vite.config.js
@@ -1,11 +1,14 @@
+const packageJson = require('../package.json');
+
 module.exports = function(config) {
   // Custom vite config here
   const output = config?.build?.rollupOptions?.output;
+  const external = config?.build?.rollupOptions?.external || [];
   output.map((obj) => {
     if(obj.sourcemap) {
       obj.sourcemap = false;
     }
   })
-  // config.build.sourcemap = false;
+  external.push(...Object.keys(packageJson.dependencies))
   return config;
 };
